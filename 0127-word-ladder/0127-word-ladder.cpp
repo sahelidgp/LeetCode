439d9df4b280fb531835231d9069aa1aff8d3144
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-06-26 00:21:19 +0530

    Time: 51 ms (85.7%), Space: 21.2 MB (65.19%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+      queue<pair<string,int>>q;
+      q.push({beginWord, 1});
+      unordered_set<string> st(wordList.begin(),wordList.end());
+      st.erase(beginWord);
+       
+      while(!q.empty()){
+        string word = q.front().first;
+        int steps = q.front().second;
+        q.pop();
+        if(word == endWord)return steps;
+        for(int i=0;i<word.size();i++){
+            char original = word[i];
+            //N x word length x26 x log N for ordered set
+            for(char ch = 'a';ch <= 'z'; ch++){
+                word[i] = ch;
+                //if exists in the set
+                if(st.find(word) != st.end()){
+                    st.erase(word);
+                    q.push({word,steps+1});
+                }
+            }
+            word[i]=original;
+        }
+      }
+      return 0;
+    }
+};
+
+// ✅ Key Points:
+// Complexity:
+// Time: O(N * L * 26)
+
+// N = number of words in the wordList
+
+// L = length of each word
+
+// Space: O(N) for the queue and set.
