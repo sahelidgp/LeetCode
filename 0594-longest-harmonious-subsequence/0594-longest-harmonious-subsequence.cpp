cb9c9fbc361829baac0566d97a2b57aed7e035a2
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-06-30 13:13:34 +0530

    Update 0594-longest-harmonious-subsequence.cpp

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -5,6 +5,30 @@ public:
         int n = nums.size();
         int i = 0;
         int maxcount = 0;
+        //tc : O(n)
+        for (int j = 0; j < n; j++) {
+            // Shrink window if difference exceeds 1
+            while (nums[j] - nums[i] > 1) {
+                i++;
+            }
+
+            // Valid window if difference is exactly 1
+            if (nums[j] - nums[i] == 1) {
+                maxcount = max(maxcount, j - i + 1);
+            }
+        }
+
+        return maxcount;
+    }
+};
+//another approach O(nlogn)
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = 0;
+        int maxcount = 0;
 
         for (int j = 0; j < n; j++) {
             // Shrink window if difference exceeds 1
@@ -21,3 +45,4 @@ public:
         return maxcount;
     }
 };
+
