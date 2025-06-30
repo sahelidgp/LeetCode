be2355555c6f394399b9b209b097422a10c25e88
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-01 01:27:35 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,18 @@
 class Solution {
-private:
-    int memo(int i,vector<int>&nums,vector<int>&dp){
-        //base case
-        if(i == 0)return nums[i]; // 2->0 so it will be better if we rob this house
-        if(i <0)return 0; // 1-> -1 not possible case
-        if(dp[i] != -1)return dp[i];
-        int pick = nums[i] + memo(i-2,nums,dp);
-        int notPick = memo(i-1,nums,dp);
-
-        return dp[i] = max(pick,notPick);
-    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int>dp(n,-1);
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
 
-        return memo(n-1,nums,dp);
+        for(int i=1;i<n;i++){
+            int take = nums[i]; 
+            if(i>1)take += dp[i-2];
+            int nottake = dp[i-1];
 
+            dp[i] = max(take,nottake);
+        }
+        return dp[n-1];
     }
 };
