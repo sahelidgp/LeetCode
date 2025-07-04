8827a076bc1923f665423663f2ea5bb37f4c1449
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-04 19:37:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (90.89%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        return 'a' + __builtin_popcount(k - 1);
+        int ans = 0;
+        int t;
+        while (k != 1) {
+            t = __lg(k);
+            if ((1 << t) == k) {
+                t--;
+            }
+            k = k - (1 << t);
+            ans++;
+        }
+        return 'a' + ans;
     }
 };
