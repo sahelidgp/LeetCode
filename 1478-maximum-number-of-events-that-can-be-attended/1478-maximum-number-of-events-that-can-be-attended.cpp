15912dc183d94fa01f8504e4d77d84daf4dc7b92
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-08 01:47:32 +0530

    Time: 60 ms (69.14%), Space: 75 MB (39.16%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -2,27 +2,42 @@ class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
         int n = events.size();
-        int maxDay = 0;
-        for (int i = 0; i < events.size(); i++) {
-            maxDay = max(maxDay, events[i][1]);
-        }
-        priority_queue<int, vector<int>, greater<>> pq;
-        sort(events.begin(), events.end());
-        int ans = 0;
-        for (int i = 0, j = 0; i <= maxDay; i++) {
-            while (j < n && events[j][0] <= i) {
-                pq.emplace(events[j][1]);
-                j++;
-            }
-            while (!pq.empty() && pq.top() < i) {
-                pq.pop();
-            }
-            if (!pq.empty()) {
-                pq.pop();
-                ans++;
-            }
-        }
+        sort(begin(events),end(events)); // nlogn
 
-        return ans;
+        priority_queue<int,vector<int>,greater<int>>pq; //min-heap
+
+        int day = events[0][0];
+        int i = 0;
+        int count = 0;//result number of events attended 
+        
+        //push and pop for every events attended
+        //push pop
+        //n*logn
+        while(!pq.empty() || i < n){
+
+            if(pq.empty())
+            day = events[i][0];
+            while(i<n && events[i][0] == day){
+                pq.push(events[i][1]);
+                i++;
+            }
+        
+        if(!pq.empty()){
+        pq.pop(); // 1 event attended on this day
+        count++; // counting the result
+        }
+        day++;
+
+        //skip those events whose endDay < day
+        while(!pq.empty() && pq.top()<day){
+            pq.pop();
+        }
+        }
+        return count;
     }
 };
+/*
+for any day if I have an option to select among events ,I will choose the one that ends earlier
+
+skip the events whose endday < day
+*/
