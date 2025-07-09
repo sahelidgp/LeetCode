ace2385e6cd79fb48d16cd219f15baa4a08bebd8
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-10 04:54:26 +0530

    Time: 4 ms (7.89%), Space: 13.2 MB (31.22%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,28 @@
 class Solution {
-private:
-    int f(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        if(i == n-1 )return triangle[i][j];
-        if(i>=n || j>i+1)return 1e9;
-        if(dp[i][j] != -1)return dp[i][j];
-        int down = f(i+1,j,n,triangle,dp);
-        int diag = f(i+1,j+1,n,triangle,dp);
-
-        return dp[i][j] = min(down,diag)+triangle[i][j];
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         int m = triangle[n-1].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(0,0,n,triangle,dp);
+
+        
+        vector<vector<int>>dp(n,vector<int>(m,1e9));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                if(i == 0 && j == 0)dp[i][j] = triangle[i][j];
+                else{
+                    int up = 1e9;
+                    int diag = 1e9;
+                    if(i>0) up = dp[i-1][j];
+                    if(i>0 && j>0)diag = dp[i-1][j-1];
+
+                    dp[i][j] = min(up,diag)+triangle[i][j];
+                }
+            }
+        }
+        int mini = 1e9;
+        for(int p=0;p<m;p++){
+            mini = min(mini,dp[n-1][p]);
+        }
+        return mini;
     }
 };
