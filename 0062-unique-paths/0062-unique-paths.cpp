0a7dfa216b1ddc464f2a5ef783cfca6e0205044a
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-09 20:23:39 +0530

    Time: 0 ms (100%), Space: 9.3 MB (33.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,23 @@
+//tabulation
 class Solution {
-private:
-    int memo(int i,int j,vector<vector<int>>& dp){
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(i < 0 || j < 0)return 0;
-        if(dp[i][j] != -1)return dp[i][j];
-        int up = memo(i-1,j,dp);
-        int left = memo(i,j-1,dp);
-        return dp[i][j] = up+left;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return memo(m-1,n-1,dp);
-    }
+        //dp vector
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int up = 0;
+                int left = 0;
+                if(i == 0 && j == 0)dp[i][j] = 1;
+                else{
+                if(j>0)left = dp[i][j-1];
+                if(i>0)up = dp[i-1][j];
+                dp[i][j] = left+up;
+                }
 
+            }
+        }
+        return dp[m-1][n-1];
+    }
 };
