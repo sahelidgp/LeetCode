1e549438b636d7aba7a7bf0854b6e5f4c978fb2e
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-12 03:12:24 +0530

    Time: 3 ms (32.53%), Space: 14.9 MB (76.88%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
+
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp = matrix; // dp[i][j] = min sum to reach cell (i,j)
+        vector<vector<int>>dp(n,vector<int>(n));
+        //base case
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = matrix[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<n;j++){
+                int down = dp[i+1][j];
+                int rightdiag = (j<n-1)?dp[i+1][j+1]:1e9;
+                int leftdiag = (j>0)?dp[i+1][j-1]:1e9;
 
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < n; j++) {
-                int down = dp[i + 1][j];
-                int left = (j > 0) ? dp[i + 1][j - 1] : INT_MAX;
-                int right = (j < n - 1) ? dp[i + 1][j + 1] : INT_MAX;
-
-                dp[i][j] += min({down, left, right});
+                dp[i][j] = matrix[i][j] + min({down,rightdiag,leftdiag});
             }
         }
-
-        return *min_element(dp[0].begin(), dp[0].end());
+        return *min_element(dp[0].begin(),dp[0].end());
     }
 };
