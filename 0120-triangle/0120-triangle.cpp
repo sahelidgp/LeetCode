19da583f19b7393fb47194715a40f1355981dee2
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-11 21:39:05 +0530

    Time: 0 ms (100%), Space: 12.8 MB (73.75%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,20 +4,22 @@ public:
         int n = triangle.size();
         int m = triangle[n-1].size();
 
-        vector<vector<int>>dp(n,vector<int>(m));
+        vector<int>dp(m);
 
         for(int j=0;j<m;j++){
-            dp[n-1][j] = triangle[n-1][j];
+            dp[j] = triangle[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
+            vector<int>temp(m);
             for(int j=i;j>=0;j--){
 
-                int down = triangle[i][j] + dp[i+1][j];
-                int diag = triangle[i][j] + dp[i+1][j+1];
+                int down = triangle[i][j] + dp[j];
+                int diag = triangle[i][j] + dp[j+1];
 
-                dp[i][j] = min(down,diag);
+                temp[j] = min(down,diag);
             }
+            dp = temp;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
