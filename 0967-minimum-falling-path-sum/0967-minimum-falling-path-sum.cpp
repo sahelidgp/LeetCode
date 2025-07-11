cf33dec1c90a46e8de9447f31397b490fd814533
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-07-12 03:15:58 +0530

    Time: 0 ms (100%), Space: 14.9 MB (60.99%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -3,20 +3,22 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n));
+        vector<int>dp(n);
         //base case
         for(int j=0;j<n;j++){
-            dp[n-1][j] = matrix[n-1][j];
+            dp[j] = matrix[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
+            vector<int>temp(n);
             for(int j=0;j<n;j++){
-                int down = dp[i+1][j];
-                int rightdiag = (j<n-1)?dp[i+1][j+1]:1e9;
-                int leftdiag = (j>0)?dp[i+1][j-1]:1e9;
+                int down = dp[j];
+                int rightdiag = (j<n-1)?dp[j+1]:1e9;
+                int leftdiag = (j>0)?dp[j-1]:1e9;
 
-                dp[i][j] = matrix[i][j] + min({down,rightdiag,leftdiag});
+                temp[j] = matrix[i][j] + min({down,rightdiag,leftdiag});
             }
+            dp = temp;
         }
-        return *min_element(dp[0].begin(),dp[0].end());
+        return *min_element(dp.begin(),dp.end());
     }
 };
